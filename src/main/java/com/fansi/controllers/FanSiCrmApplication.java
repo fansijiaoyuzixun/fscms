86b20039247bc7dc1883459962a8e0cdbea073e9
fansijiaoyuzixun <60516439+fansijiaoyuzixun@users.noreply.github.com>
2020-02-03 01:14:54 +0800

    生成war

--- src/main/java/com/fansi/controllers/FanSiCrmApplication.java
+++ src/main/java/com/fansi/controllers/FanSiCrmApplication.java
@@ -1,16 +1,15 @@
-package com.fansi.demo;
+package com.fansi.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FanSiCrmApplication {
+public class FanSiCrmApplication  {
 
 	/*
 	 * 程序入口
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FanSiCrmApplication.class, args);
-	}
-
+	}		
 }
